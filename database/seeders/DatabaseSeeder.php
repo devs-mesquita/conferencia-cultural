7e0ec0d15f6d7474a27b0b72fb9e7feede682b7e
felipe-vdl <felipe.vidal@mesquita.rj.gov.br>
2023-10-19 14:21:22 +0000

    Docker, Apache, Upsert Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,39 +15,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
+        DB::table('users')->firstOrCreate(["name" => "Victor Mussel"], [
             'name' => 'Victor Mussel',
             'email' => 'victor.mussel@hotmail.com',
             'nivel' => 'Avaliador',
             'password' => bcrypt('secret')
         ]);
 
-        DB::table('gts')->insert([
-            [
+        DB::table('gts')->firstOrCreate(["nome" => "GT 1. Institucionalização, Marcos Legais e Sistema Nacional de Cultura"], [
                 "nome" => "GT 1. Institucionalização, Marcos Legais e Sistema Nacional de Cultura",
                 "qtd"  => "30",
                 "color"=> "#ebeb05",
-            ],
+            ]);
+
+        DB::table('gts')->firstOrCreate(["nome" => "GT 2. Democratização do Acesso à Cultura e Participação Social"],
             [
                 "nome" => "GT 2. Democratização do Acesso à Cultura e Participação Social",
                 "qtd"  => "30",
                 "color"=> "#eb0505",                
-            ],
+            ]);
+
+        DB::table('gts')->firstOrCreate(["nome" => "GT 3. Identidade, Patrimônio, Memória, Direito às Artes e Linguagens Digitais"],
             [
                 "nome" => "GT 3. Identidade, Patrimônio, Memória, Direito às Artes e Linguagens Digitais",
                 "qtd"  => "30",
                 "color"=> "#eb05cf",                
-            ],
+            ]);
+
+        DB::table('gts')->firstOrCreate(["nome" => "GT 4. Diversidade Cultural e Transversalidade de Gênero, Raça e Acessibilidade na Política Cultural"],
             [
                 "nome" => "GT 4. Diversidade Cultural e Transversalidade de Gênero, Raça e Acessibilidade na Política Cultural",
                 "qtd"  => "30",
                 "color"=> "#0521eb",                
-            ],
+            ]);
+
+        DB::table('gts')->firstOrCreate(["nome" => "GT 5. Economia Criativa, Trabalho, Renda e Sustentabilidade"],
             [
                 "nome" => "GT 5. Economia Criativa, Trabalho, Renda e Sustentabilidade",
                 "qtd"  => "30",
                 "color"=> "#05eb2c",                
-            ],
-        ]);
+            ]);
     }
 }
---
+++ scripts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+php artisan key:generate && \
+php artisan config:cache &&\
+php artisan migrate --force && \
+php artisan db:seed && \
+apache2ctl -D FOREGROUND
